f1b846fa0d041888b96052c933ff64d2dbb78260
Anastasija <anastasija.sergeevnna23@gmail.com>
2023-01-28 17:05:33 +0300

    add task6

---
+++ task№6/Program.cs
@@ -0,0 +1,17 @@
+﻿// №6. Написать программу, которая на вход принимает число и выдает, является ли число четным
+// например:
+// 4->да;
+// 5->нет
+
+Console.WriteLine("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+ if (number1 % 2 == 0)
+    {
+        Console.WriteLine(number1 + " - Четное число. ");
+    }
+    else
+    {
+        Console.WriteLine(number1 + " - Нечетное число. ");
+    
+    }
+

dded909aa655602d23bffe68e1cc6658be3a4d0c
Anastasija <anastasija.sergeevnna23@gmail.com>
2023-01-28 17:58:23 +0300

    add task8

---
+++ task№8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число: ");
+int N = int.Parse(Console.ReadLine()); 
+int start = 1;
+while (start<=N) 
+    {
+         if (start % 2 ==0)
+         {
+            Console.Write(start + ", ");
+         }
+     ++start;
+    }

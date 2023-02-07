2d6d6f9c50a2b7a65bee98c78d9460a88c6160ce
Anastasija <anastasija.sergeevnna23@gmail.com>
2023-02-07 15:34:39 +0300

    add task21

--- task№21/Program.cs
+++ task№21/Program.cs
@@ -4,122 +4,24 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.Clear();
-Console.WriteLine("Введите координату x1 ");
-long x1 = long.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату y1 ");
-long y1 = long.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату z1 ");
-long z1 = long.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату x2 ");
-long x2 = long.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату y2 ");
-long y2 = long.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату z2 ");
-long z2 = long.Parse(Console.ReadLine());
-long length;
+Console.Write("Введите координату точки x1: ");
+long x1 = long.Parse(Console.ReadLine()!);
+Console.Write("Введите координату точки у1: ");
+long y1 = long.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату точки z1: ");
+long z1 = long.Parse(Console.ReadLine()!);
+Console.Write("Введите координату точки x2: ");
+long x2 = long.Parse(Console.ReadLine()!);
+Console.Write("Введите координату точки y2: ");
+long y2 = long.Parse(Console.ReadLine()!);
+Console.Write("Введите координату точки z2: ");
+long z2 = long.Parse(Console.ReadLine()!);
 
-if ((x1>0) &&  (x2>0))
-{
-    if(x1>x2)
-    {
-     length = x1 - x2;
-    }
-     else
-     {
-        length = x2 - x1;
-     }
-}
-       else if ((x1<0) && (x2<0))
-       {
-     if(x1>x2)
-     {
-     length = x2 - x1;
-    }
-    else
-    {
-        length = x1- x2;
-    }
-       }
-       else
-       {
-       if(x1>x2)
-     {
-     length = x2 - x1;
-    }
-    else
-    {
-        length = x2- x1;
-    }
-       }
-    long width;
-    if ((y1>0) &&  (y2>0))
-{
-    if(y1>y2)
-    {
-     width = y1 - y2;
-    }
-     else
-     {
-        width = y2 - y1;
-     }
-}
-       else if ((y1<0) && (y2<0))
-       {
-     if(y1>y2)
-     {
-     width = y2 - y1;
-    }
-    else
-    {
-        width = y1- y2;
-    }
-       }
-       else
-       {
-       if(y1>y2)
-     {
-    width = y2 - y1;
-    }
-    else
-    {
-        width = y2- y1;
-    }
-       }
-    
-    long heidht;
-    if ((z1>0) &&  (z2>0))
-{
-    if(z1>z2)
-    {
-     heidht = z1 - z2;
-    }
-     else
-     {
-        heidht = z2 - z1;
-     }
-}
-       else if ((z1<0) && (z2<0))
-       {
-     if(z1>z2)
-     {
-     heidht= z2 - z1;
-    }
-    else
-    {
-       heidht = z1- z2;
-    }
-       }
-       else
-       {
-       if(z1>z2)
-     {
-    heidht = z2 - z1;
-    }
-    else
-    {
-        heidht = z2- z1;
-    }
-       }
-    
-double result = Math.Round(Math.Sqrt(length*length+width*width+heidht*heidht),3);
+long A= x2 - x1;
+long B = y2 - y1;
+ long C = z1 - z2;
+
+double result = Math.Round(Math.Sqrt(A * A + B * B + C * C),2);
 Console.WriteLine($"расстояние между точками {result}");
+
+
